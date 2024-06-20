c5bc2d03274e645e69a57a76a9d27fcff8689df9
Нарек Антонян <narekantonyan2033@gmail.com>
2024-06-20 18:04:37 +0300

    Добавьте файлы проекта.

---
+++ z9/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static string ReverseString(string input)
+    {
+        char[] charArray = input.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+
+    static void Main()
+    {
+        string testString = "Hello, world!";
+        Console.WriteLine($"Обратная строка: {ReverseString(testString)}");
+    }
+}
